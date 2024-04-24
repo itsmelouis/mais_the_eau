b72cc3d9ba1d33c66a4a2709bccc161e2d364f9a
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-04-24 15:15:22 +0200

    feat: add dotenv for api key

--- lib/api/weather.dart
+++ lib/api/weather.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 class OpenWeatherAPI {
-  String apiKey = 'b31b8b572f8771e623ebfb0a3a60055c';
+  String apiKey = dotenv.env['API_KEY']!;
 
   String apiUrl(lat, lon) {
     return 'https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
