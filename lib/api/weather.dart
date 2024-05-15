dd5c525816dddaa99298b3b98c495602f8e36dab
distrib-ps <theodelhay@orange.fr>
2024-05-15 11:14:14 +0200

    feat:create project with weathermap

--- lib/api/weather.dart
+++ lib/api/weather.dart
@@ -1,8 +1,5 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-
 class OpenWeatherAPI {
-  String apiKey = dotenv.env['API_KEY']!;
+  String apiKey = 'b31b8b572f8771e623ebfb0a3a60055c';
 
   String apiUrl(lat, lon) {
     return 'https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
