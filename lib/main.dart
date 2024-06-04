aeff27a8ed9b6d53fee1849e11342b1a3bc5fc47
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-06-04 21:51:06 +0200

    feat: add navbar to the main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mais_the_eau/screens/home_page.dart';
-import 'package:mais_the_eau/screens/forecasts_page.dart';
 import 'package:mais_the_eau/screens/bookmarks_page.dart';
 import 'package:mais_the_eau/screens/glossary_page.dart';
+import 'package:mais_the_eau/screens/settings_page.dart';
 import 'package:mais_the_eau/theme/app_theme.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,12 +21,67 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const HomePage(),
-      routes: {
-        '/forecasts': (context) => const ForecastsPage(),
-        '/bookmarks': (context) => const BookmarksPage(),
-        '/glossary': (context) => const GlossaryPage(),
-      },
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const BookmarksPage(),
+    const GlossaryPage(),
+    const SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          SalomonBottomBarItem(
+            icon: Icon(Icons.cloud),
+            title: Text("Prévisions"),
+            selectedColor: Colors.pink,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.favorite_border),
+            title: Text("Favoris"),
+            selectedColor: Colors.orange,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.book),
+            title: Text("Glossaire"),
+            selectedColor: Colors.teal,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("Paramètres"),
+            selectedColor: Colors.teal,
+          ),
+        ],
+      ),
     );
   }
 }
